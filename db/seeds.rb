07fe4e1f19567ce3d83924749d6b90f85a69180e
Suguru Omura <omura.suguru@dena.jp>
2016-11-21 20:50:52 +0900

    fix for rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database
+#   with its default values.
+# The data can then be loaded with the rails db:seed command
+#   (or created alongside the database with db:setup).
 #
 # Examples:
-#
+# rubocop:disable Style/LineLength
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# rubocop:enable Style/LineLength
 #   Character.create(name: 'Luke', movie: movies.first)
