48d5c98ecf5637702e10a5267ca13a13630290e0
Suguru Omura <omura.suguru@dena.jp>
2016-11-22 23:26:35 +0900

    jenkins_test

--- script/jenkins/run_rubocop.sh
+++ script/jenkins/run_rubocop.sh
@@ -4,4 +4,5 @@ bundle install --path vendor/bundle
 bundle exec rubocop \
   --require rubocop/formatter/checkstyle_formatter \
   --format RuboCop::Formatter::CheckstyleFormatter -o reports/xml/checkstyle-result.xml \
-  --format html -o reports/html/index.html || true
+  --format html -o reports/html/index.html \
+  --format RuboCop::Formatter::CheckstyleFormatter --no-color --rails --out tmp/checkstyle.xml || true
