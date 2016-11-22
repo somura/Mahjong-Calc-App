b203061fb74b54c34e783ebc2f6f61b88b66dfbe
Suguru Omura <omura.suguru@dena.jp>
2016-11-22 23:03:45 +0900

    change script

--- script/jenkins/run_rubocop.sh
+++ script/jenkins/run_rubocop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 bundle install --path vendor/bundle
-bundle exec rubocop
+bundle exec rubocop --require rubocop/formatter/checkstyle_formatter --format RuboCop::Formatter::CheckstyleFormatter --no-color --rails --out tmp/checkstyle.xml
