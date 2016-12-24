547dad7438e5239118fb9d9beeb562418f569d64
Suguru Omura <omura.suguru@dena.jp>
2016-12-24 14:47:58 +0900

    fix template

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,7 @@
 class GameController < ApplicationController
   def index
-    tournament_id = params['tournament_id']
-    members = TournamentUser.where(tournament_id: tournament_id)
+    @tournament_id = params['tournament_id']
+    members = TournamentUser.where(tournament_id: @tournament_id)
     @members = members.each_with_object([]) do |(member), array|
       user = User.find(member.user_id)
       array << user
@@ -18,11 +18,24 @@ class GameController < ApplicationController
         end
         _array << result
       end
-      array << data
+      array << { game_id: game.id, data: data }
     end
 
+    @data = @members.each_with_object([]) do |(member), array|
+      i = 0
+      point = 0
+      data = games.each_with_object([]) do |(game), _array|
+        i += 1
+        game_user = GameUser.where(game_id: game.id, user_id: member.id)
+        point += game_user.first.point unless game_user.empty?
+        _array << [i, point]
+      end
+      array << { name: member.login_id, data: data }
+    end
+    p @data
+
     @results = members.each_with_object([]) do |(member), array|
-      game_users = GameUser.where(tournament_id: tournament_id, user_id: member.user_id)
+      game_users = GameUser.where(tournament_id: @tournament_id, user_id: member.user_id)
       array << game_users.map {|game_user| game_user.point.to_i }.inject(:+)
     end
     render template: 'game/index'
@@ -101,6 +114,106 @@ class GameController < ApplicationController
       game_user_data = {
         tournament_id: tournament.id,
         game_id:       game.id,
+        user_id:       result[:user],
+        score:         result[:score],
+        point:         result[:point],
+        rank:          i + 1,
+        position:      result[:position],
+      }
+      game_user = GameUser.new game_user_data
+      game_user.save
+    end
+
+    redirect_to action: 'index'
+  end
+
+  def edit
+    @tournament_id = params['tournament_id']
+    @game_id       = params['id']
+
+    members = TournamentUser.where(tournament_id: @tournament_id)
+    @members = members.each_with_object([]) do |(member), array|
+      user = User.find(member.user_id)
+      array << [ user.login_id, member.user_id ]
+    end
+
+    game_users = GameUser.where(game_id: @game_id)
+    @game_users = [1, 2, 3, 4].each_with_object([]) do |(i), array|
+      game_user = game_users.select {|g| g.position.to_i == i }
+      user = User.find(game_user.first.user_id)
+      array << { user: user.id, score: game_user.first.score }
+    end
+
+    render template: 'game/edit'
+  end
+
+  def update
+    @game_id = params['id']
+    user1 = params['position1']
+    user2 = params['position2']
+    user3 = params['position3']
+    user4 = params['position4']
+    score1 = params['score1'].to_i
+    score2 = params['score2'].to_i
+    score3 = params['score3'].to_i
+    score4 = params['score4'].to_i
+
+    return redirect_to action: 'new', status: 401 unless user1
+    return redirect_to action: 'new', status: 402 unless user2
+    return redirect_to action: 'new', status: 403 unless user3
+    return redirect_to action: 'new', status: 404 unless user4
+    return redirect_to action: 'new', status: 405 unless score1
+    return redirect_to action: 'new', status: 406 unless score2
+    return redirect_to action: 'new', status: 407 unless score3
+    return redirect_to action: 'new', status: 408 unless score4
+
+    tournament = Tournament.find(params['tournament_id'])
+
+    results = [
+      { user: user4, score: score4, position: 4 },
+      { user: user3, score: score3, position: 3 },
+      { user: user2, score: score2, position: 2 },
+      { user: user1, score: score1, position: 1 },
+    ]
+
+    if results.map {|r| r[:user] }.uniq.size != 4
+      return redirect_to action: 'new', status: 409
+    end
+    if results.map {|r| r[:score] }.inject(:+) != 100000
+      return redirect_to action: 'new', status: 410
+    end
+
+    results = results.sort_by {|r| r[:score] }.reverse
+    results.each_with_index do |result, i|
+      point = result[:score] / 1000
+      if point > 0
+        if (point - point.to_i) >= 0.6
+          point = (point + 1).to_i
+        else
+          point = point.to_i
+        end
+      else
+        if (point - point.to_i).abs >= 0.6
+          point = (point - 1).to_i
+        else
+          point = point.to_i
+        end
+      end
+      point += tournament.send("uma#{i + 1}") - (tournament.return_score.to_i / 1000)
+      results[i][:point] = point
+    end
+    diff = results.map {|r| r[:point] }.inject(:+)
+    results[0][:point] -= diff
+
+    game_users = GameUser.where(game_id: @game_id)
+    game_users.each do |game_user|
+      game_user.destroy
+    end
+
+    results.each_with_index do |result, i|
+      game_user_data = {
+        tournament_id: tournament.id,
+        game_id:       @game_id,
         user_id:       result[:user],
         score:         result[:score],
         point:         result[:point],
--- app/controllers/tournament_controller.rb
+++ app/controllers/tournament_controller.rb
@@ -47,4 +47,59 @@ class TournamentController < ApplicationController
 
     redirect_to action: 'index'
   end
+
+  def edit
+    @tournament_id = params['id']
+    @setting = Tournament.find(@tournament_id)
+    tournament_users = TournamentUser.where(tournament_id: @tournament_id)
+    @members = tournament_users.each_with_object({}) do |(user), hash|
+      hash["#{user.user_id}"] = true
+    end
+    p @members
+
+    friends = Friend.where(user_id: user_id)
+    @friend_data = friends.each_with_object([]) do |(friend), array|
+      array << User.find(friend.friend_user_id)
+    end
+    render template: 'tournament/edit'
+  end
+
+  def update
+    tournament_id = params['id']
+
+    return redirect_to action: 'index', status: 400 unless params['mode']
+    return redirect_to action: 'index', status: 400 unless params['def_score']
+    return redirect_to action: 'index', status: 400 unless params['return_score']
+    return redirect_to action: 'index', status: 400 unless params['return_score']
+    uma = [ params['uma1'], params['uma2'], params['uma3'], params['uma4'] ].map {|m| m.to_i }
+    unless uma.inject {|sum, n| sum + n } == 0
+      return redirect_to action: 'index', status: 400
+    end
+    return redirect_to action: 'index', status: 400 if params['members'].size < 3
+    tournament = Tournament.find(tournament_id)
+    tournament.mode         = params['mode']
+    tournament.def_score    = params['def_score']
+    tournament.return_score = params['return_score']
+    tournament.uma1         = uma[0]
+    tournament.uma2         = uma[1]
+    tournament.uma3         = uma[2]
+    tournament.uma4         = uma[3]
+    tournament.point_rate   = params['point_rate']
+    tournament.tip_rate     = params['tip_rate']
+    tournament.tobi_point   = params['tobi_point']
+    tournament.save
+
+    tournament_users = TournamentUser.where(tournament_id: tournament_id)
+    tournament_users.each do |user|
+      user.destroy
+    end
+
+    (params['members'] + [user_id]).each do |member_id|
+      tournament_user_data = { tournament_id: tournament.id, user_id: member_id }
+      tournament_user = TournamentUser.new tournament_user_data
+      tournament_user.save
+    end
+
+    redirect_to action: 'index'
+  end
 end
