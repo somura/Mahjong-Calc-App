e2a7f5482638f9c52ade29442552712db7b33ee0
Suguru Omura <omura.suguru@dena.jp>
2016-12-27 22:35:58 +0900

    calc point

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+function calc1() {
+  var point = 100000 - Number($("#score2").val()) - Number($("#score3").val()) - Number($("#score4").val());
+  $("#score1").val(point);
+};
+
+function calc2() {
+  var point = 100000 - Number($("#score3").val()) - Number($("#score4").val()) - Number($("#score1").val());
+  $("#score2").val(point);
+};
+
+function calc3() {
+  var point = 100000 - Number($("#score4").val()) - Number($("#score1").val()) - Number($("#score2").val());
+  $("#score3").val(point);
+};
+
+function calc4() {
+  var point = 100000 - Number($("#score1").val()) - Number($("#score2").val()) - Number($("#score3").val());
+  $("#score4").val(point);
+};
